614fd236f4755e19eaed3f2665a31601a1ead1ec
Karishma Patel <karishmapatel1493@gmail.com>
2026-03-10 15:58:47 +0530

    Initial Commit

---
+++ ChatDemoApp/ChatDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatDemoAppApp.swift
+//  ChatDemoApp
+//
+//  Created by H E L Y N on 10/03/26.
+//
+
+import SwiftUI
+
+@main
+struct ChatDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatDemoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChatDemoApp
+//
+//  Created by H E L Y N on 10/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
